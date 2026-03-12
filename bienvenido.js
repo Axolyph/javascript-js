bc71f0138dfbffcd663f6265c59e6e62273d2d4e
Fabian Andre <axolyph@gmail.com>
2026-03-12 15:43:15 -0500

    Equipo, he realizado el 1 ejercicio de la actividad.

---
+++ bienvenido.js
@@ -0,0 +1,10 @@
+// --- EJERCICIO 1 ---
+// Usamos prompt() para pedir datos y let para almacenarlos
+let nombre = prompt("Ingresa tu nombre:");
+let edad = prompt("Ingresa tu edad:");
+
+// Mensaje de bienvenida con alert()
+alert("Â¡Bienvenido/a, " + nombre + "!");
+
+// ImpresiÃ³n en consola
+console.log("El usuario " + nombre + " tiene " + edad + " aÃ±os");
